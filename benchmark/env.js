1d4d142cb8af8949d4d96424f1608fec7d9220f1
killa <killa123@126.com>
2020-10-29 11:57:42 +0800

    opt: pre read debug env before compile

---
+++ benchmark/env.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const Benchmark = require('benchmark');
+const benchmarks = require('beautify-benchmark');
+const suite = new Benchmark.Suite();
+
+const HESSIAN_COMPILE_DEBUG = process.env.HESSIAN_COMPILE_DEBUG;
+let envVal;
+// add tests
+suite
+  .add('read env', function() {
+    envVal = process.env.HESSIAN_COMPILE_DEBUG;
+  })
+  .add('pre read', function() {
+    envVal = HESSIAN_COMPILE_DEBUG;
+  })
+  .on('cycle', function(event) {
+    benchmarks.add(event.target);
+  })
+  .on('start', function() {
+    console.log('\n  node version: %s, date: %s\n  Starting...', process.version, Date());
+  })
+  .on('complete', function done() {
+    benchmarks.log();
+  })
+  .run({ async: false });
