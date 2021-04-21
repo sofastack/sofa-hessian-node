9524ee3ddcb60f811fc999a1b08e1298084bd99b
killagu <killa123@126.com>
2021-04-21 14:48:53 +0800

    fix: fix complex array

--- test/util.test.js
+++ test/util.test.js
@@ -18,4 +18,27 @@ describe('test/util.test.js', () => {
     id = utils.normalizeUniqId(info, '1.0');
     assert(id === 'java.util.List#java.util.List#com.sofa.testObject#1.0');
   });
+
+  it('should handle type is object', () => {
+    const info = {
+      isArray: true,
+      type: {
+        type: 'com.sofa.testObject',
+      },
+    };
+    const id = utils.normalizeUniqId(info, '2.0');
+    assert(id === '[com.sofa.testObject#2.0');
+
+    const info2 = {
+      isArray: true,
+      type: {
+        type: 'com.sofa.testObject',
+        generic: [
+          { type: 'java.lang.String' },
+        ],
+      },
+    };
+    const id2 = utils.normalizeUniqId(info2, '2.0');
+    assert(id2 === '[com.sofa.testObject#java.lang.String#2.0');
+  });
 });
